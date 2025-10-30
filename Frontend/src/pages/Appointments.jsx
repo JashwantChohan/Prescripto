92006302b644835e1931f7cbe2259fb3b49075f7
JashwantChohan <116147134+JashwantChohan@users.noreply.github.com>
2025-10-30 17:20:41 +0500

    changes

--- Frontend/src/pages/Appointments.jsx
+++ Frontend/src/pages/Appointments.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+import { assets } from '../assets/assets_frontend/assets';
 
 function Appointments() {
-  return (
+
+  const { docId } = useParams();
+  const { doctors } = useContext(AppContext);
+
+  const [docInfo, setDocInfo] = useState(null);
+
+  const fetchDocInfo = () => {
+    const docInfo = doctors.find(doc => doc._id === docId);
+    setDocInfo(docInfo);
+    console.log(docInfo)
+  }
+
+  useEffect(() => {
+    fetchDocInfo();
+  }, [docId, doctors]);
+
+
+  return docInfo && (
     <div>
-      
+      {/* --------- doctors details ------------ */}
+      <div className='flex flex-col sm:flex-row gap-4'>
+        <div>
+          <img className='bg-primary w-full sm:max-w-72 rounded-lg' src={docInfo.image} alt="" />
+        </div>
+
+        <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 -mt-20 sm:mt-0'>
+          {/* doctor's info */}
+          <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>
+            {docInfo.name}
+            <img className='w-5' src={assets.verified_icon} alt="" />
+          </p>
+          <div className='flex items-center gap-2 text-sm mt-1 text-gray-600'>
+            <p>{docInfo.degree} - {docInfo.speciality}</p>
+            <button className='py-0.5 px-2 border text-xs rounded-full'>{docInfo.experience}</button>
+          </div>
+
+          {/* Doctor about  */}
+          <div >
+            <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>
+              About <img src={assets.info_icon} alt="" />
+            </p>
+            <p className='text-sm text-gray-500 max-w-[700px] mt-1'>{docInfo.about}</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
