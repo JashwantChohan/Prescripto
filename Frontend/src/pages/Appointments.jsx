a047db90c16bf7be3f2b00b782bf22ad931a6f98
JashwantChohan <116147134+JashwantChohan@users.noreply.github.com>
2025-10-31 16:59:59 +0500

    changes
    s

--- Frontend/src/pages/Appointments.jsx
+++ Frontend/src/pages/Appointments.jsx
@@ -7,20 +7,78 @@ import { assets } from '../assets/assets_frontend/assets';
 function Appointments() {
 
   const { docId } = useParams();
-  const { doctors } = useContext(AppContext);
+  const { doctors, currencySymbol } = useContext(AppContext);
+
+  const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
   const [docInfo, setDocInfo] = useState(null);
+  const [docSlot, setDocSlot] = useState([]);
+  const [slotIndex, setSlotIndex] = useState(0);
+  const [slotTime, setSlotTime] = useState('');
 
   const fetchDocInfo = () => {
     const docInfo = doctors.find(doc => doc._id === docId);
     setDocInfo(docInfo);
-    console.log(docInfo)
+  }
+
+  const getAvailableSlots = async () => {
+    setDocSlot([])
+
+    // getting current date
+    let today = new Date();
+
+    for (let i = 0; i < 7; i++) {
+      // getting next 7 days
+      let currentDate = new Date(today);
+      currentDate.setDate(today.getDate() + i);
+
+      // setting end time of the date with index
+      let endTime = new Date();
+      endTime.setDate(today.getDate() + i);
+      endTime.setHours(21, 0, 0, 0);
+
+      // setting hours
+      if (today.getDate() === currentDate.getDate()) {
+        currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10);
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0);
+      } else {
+        currentDate.setHours(10, 0, 0, 0);
+        currentDate.setMinutes(0);
+      }
+
+      let timeSlots = [];
+
+      while (currentDate < endTime) {
+        let formattedTime = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+        // adding time slots to array
+        timeSlots.push(
+          {
+            datetime: new Date(currentDate),
+            time: formattedTime
+          }
+        );
+
+        //  increment current time by 30 minutes
+        currentDate.setMinutes(currentDate.getMinutes() + 30);
+      }
+
+      setDocSlot(prev => ([...prev, timeSlots]))
+    }
   }
 
   useEffect(() => {
     fetchDocInfo();
   }, [docId, doctors]);
 
+  useEffect(() => {
+    getAvailableSlots();
+  }, [docInfo]);
+
+
+  useEffect(() => {
+    console.log(docSlot)
+  }, [docSlot]);
 
   return docInfo && (
     <div>
@@ -48,6 +106,32 @@ function Appointments() {
             </p>
             <p className='text-sm text-gray-500 max-w-[700px] mt-1'>{docInfo.about}</p>
           </div>
+          <p className='text-gray-500 font-medium mt-4'>
+            Appointment fee: <span className='text-gray-600' >{currencySymbol}{docInfo.fees}</span>
+          </p>
+        </div>
+      </div>
+
+      {/* --------- booking slots ------------ */}
+      <div className='sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700'>
+        <p>Booking slots</p>
+        <div className='flex gap-3 items-center w-full overflow-x-scroll mt-4'>
+          {
+            docSlot.length && docSlot.map((item, index) => (
+              <div onClick={() => setSlotIndex(index)} className={` text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? 'bg-primary text-white' : 'border border-gray-200'}`} key={index}>
+                <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
+                <p>{item[0] && item[0].datetime.getDate()}</p>
+              </div>
+            ))
+          }
+        </div>
+
+        <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
+          {docSlot.length && docSlot[slotIndex].map((item, index) => (
+            <p onClick={() => setSlotTime(item.time)} className={`text-sm font-light shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white' : 'text-gray-400 border border-gray-300'}`} key={index}>
+              {item.time.toLowerCase()}
+            </p>
+          ))}
         </div>
       </div>
     </div>
