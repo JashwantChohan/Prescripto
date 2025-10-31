dee9e46b783714e4f34af6169733c14f18cc7baf
JashwantChohan <116147134+JashwantChohan@users.noreply.github.com>
2025-10-31 17:22:24 +0500

    changes

--- Frontend/src/pages/Appointments.jsx
+++ Frontend/src/pages/Appointments.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 import { assets } from '../assets/assets_frontend/assets';
+import RelatedDoctors from '../components/RelatedDoctors';
 
 function Appointments() {
 
@@ -128,12 +129,16 @@ function Appointments() {
 
         <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
           {docSlot.length && docSlot[slotIndex].map((item, index) => (
-            <p onClick={() => setSlotTime(item.time)} className={`text-sm font-light shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white' : 'text-gray-400 border border-gray-300'}`} key={index}>
+            <p onClick={() => setSlotTime(item.time)} className={`text-sm font-light shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white' : 'text-gray-600 border border-gray-300'}`} key={index}>
               {item.time.toLowerCase()}
             </p>
           ))}
         </div>
+        <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6'>Book an Appointment</button>
       </div>
+
+      {/* Listint related Doctors */}
+      <RelatedDoctors docId={docId} speciality={docInfo.speciality} />
     </div>
   )
 }
