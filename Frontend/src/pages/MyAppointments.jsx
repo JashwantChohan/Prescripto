2912be3f3ce5dd7b0987511f3cf2d84e89987547
JashwantChohan <116147134+JashwantChohan@users.noreply.github.com>
2026-02-14 16:23:35 +0500

    delete appointment feature added

--- Frontend/src/pages/MyAppointments.jsx
+++ Frontend/src/pages/MyAppointments.jsx
@@ -2,7 +2,6 @@ import { useContext, useEffect, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 import { toast } from 'react-toastify';
 import axios from 'axios';
-import appointmentModel from '../../../Backend/models/appointmentModel';
 
 function MyAppointments() {
 
@@ -68,8 +67,9 @@ function MyAppointments() {
             <div>
             </div>
             <div className='flex flex-col gap-2 justify-end'>
-              <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 mt-3 border rounded hover:bg-primary hover:text-white transiton-all duration-300'>Pay Online</button>
-              <button onClick={() => cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2  border rounded hover:bg-red-600 hover:text-white transiton-all duration-300'>Cancel Appointment </button>
+              {!item.cancelled && <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 mt-3 border rounded hover:bg-primary hover:text-white transiton-all duration-300'>Pay Online</button>}
+              {!item.cancelled && <button onClick={() => cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2  border rounded hover:bg-red-600 hover:text-white transiton-all duration-300'>Cancel Appointment </button>}
+              {item.cancelled && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500'> Appointment cancelled</button>}
             </div>
 
           </div>
